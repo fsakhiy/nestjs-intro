bb66d210d69302a4b373655f4cdaff54bb4a5e6a
sakhiy <sakhiyfairuz@gmail.com>
2023-08-28 08:55:22 +0700

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+}
+bootstrap();
