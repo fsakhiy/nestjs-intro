0c16704a4339d2ef56dd9dbd56a08b46cab65b55
sakhiy <sakhiyfairuz@gmail.com>
2023-08-28 14:43:54 +0700

    removed unused code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,25 +8,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Post()
-  getPostHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Put()
-  updateData(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('/hello')
-  getSayHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('/json')
-  getJson(): any {
-    return { name: 'you' };
   }
 }
